846ae5ca832868912501f9113f5de83ec18dbbd1
Brian Henson <brian.henson1@yahoo.com>
2020-03-27 20:15:08 -0700

    first draft of more "general cleanup" scripts

---
+++ __prune_invalid_faces.py
@@ -0,0 +1,128 @@
+# Nuthouse01 - 03/14/2020 - v3.01
+# This code is free to use and re-distribute, but I cannot be held responsible for damages that it may or may not cause.
+#####################
+
+
+
+import sys
+# first, version check: verify that this is using python3
+# i don't know if it will actually work in 3.4 but i know it will fail in any python2 version
+# actually written/tested with 3.6.6 so guaranteed to work on that or higher
+# between 3.4 and 3.6, who knows
+if sys.version_info < (3, 4):
+	print("Your version of Python is too old to run this script, please update!")
+	print("Your current version = " + sys.version)
+	print("...press ENTER to exit...")
+	input()
+	exit()
+
+# second, wrap custom imports with a try-except to catch it if files are missing
+try:
+	import nuthouse01_core as core
+	import nuthouse01_pmx_parser as pmxlib
+except ImportError as eee:
+	print(eee)
+	print("ERROR: failed to import some of the necessary files, all my scripts must be together in the same folder!")
+	print("...press ENTER to exit...")
+	input()
+	exit()
+	core = pmxlib = None
+
+
+# when debug=True, disable the catchall try-except block. this means the full stack trace gets printed when it crashes,
+# but if launched in a new window it exits immediately so you can't read it.
+DEBUG = False
+
+
+def main():
+	# TODO: ACTUALLY TEST THIS!
+	# print info to explain the purpose of this file
+	print("This script will delete any invalid faces in the model, a simple operation.")
+	# print info to explain what inputs it needs
+	print("Inputs: PMX file 'model.pmx'")
+	# print info to explain what outputs it creates
+	print("Outputs: PMX file '[model]_faceprune.pmx'")
+	print("")
+	
+	# prompt PMX name
+	print("Please enter name of PMX model file:")
+	input_filename_pmx = core.prompt_user_filename(".pmx")
+	pmx = pmxlib.read_pmx(input_filename_pmx)
+
+	#############################
+	# ready for logic
+	
+	faces_to_remove = []
+	# identify faces which need removing
+	for i,face in enumerate(pmx[2]):
+		# valid faces are defined by 3 unique vertices, if the vertices are not unique then the face is invalid
+		if len(face) != len(set(face)):
+			faces_to_remove.append(i)
+	
+	numdeleted = len(faces_to_remove)
+	prevtotal = len(pmx[2])
+	if numdeleted == 0:
+		print("No changes are required")
+		core.pause_and_quit("Done with everything! Goodbye!")
+		return None
+
+	# each material has some number of faces associated with it, those values must be changed when faces are deleted!
+	# the question simply becomes, "how many faces within range [start, end] are being deleted"
+	# the list of faces being deleted is in known-sorted order
+	faces_removed_per_material = []
+	end_del_face_idx = 0
+	start_del_face_idx = 0
+	face_id_end = 0
+	for mat in pmx[4]:
+		face_id_end += mat[25]  # this tracks the id of the final face in this material
+		# walk thru the list of faces to remove until i find one that isn't part of the current material,
+		# or until i reach the end of the list
+		while end_del_face_idx < len(faces_to_remove) and faces_to_remove[end_del_face_idx] < face_id_end:
+			end_del_face_idx += 1
+		# within the list of faces to be deleted,
+		# start_del_face_idx is the idx of the first face that falls within this material's scope,
+		# and end_del_face_idx is the idx of the first face that falls within THE NEXT material's scope
+		# therefore their difference is the number of faces to remove from the current material
+		faces_removed_per_material.append(end_del_face_idx - start_del_face_idx)
+		start_del_face_idx = end_del_face_idx
+		
+	# now, apply the changes to the size of each material
+	newsum = 0
+	for mat, loss in zip(pmx[4], faces_removed_per_material):
+		mat[25] -= loss
+		newsum += mat[25]
+		
+	# now, delete the acutal faces
+	faces_to_remove.reverse()
+	for f in faces_to_remove:
+		pmx[2].pop(f)
+	
+	assert(len(pmx[2]) == newsum)  # assert material face allocation matches the actual total number of faces
+	
+	print("Identified and deleted {} / {} = {:.1%} faces for being invalid".format(
+		numdeleted, prevtotal, numdeleted/prevtotal))
+
+	# write out
+	output_filename_pmx = "%s_faceprune.pmx" % core.get_clean_basename(input_filename_pmx)
+	output_filename_pmx = core.get_unused_file_name(output_filename_pmx)
+	pmxlib.write_pmx(pmx, output_filename_pmx)
+	
+	core.pause_and_quit("Done with everything! Goodbye!")
+	return None
+	
+
+if __name__ == '__main__':
+	print("Nuthouse01 - 03/14/2020 - v3.01")
+	if DEBUG:
+		main()
+	else:
+		try:
+			main()
+		except (KeyboardInterrupt, SystemExit):
+			# this is normal and expected, do nothing and die normally
+			pass
+		except Exception as ee:
+			# if an unexpected error occurs, catch it and print it and call pause_and_quit so the window stays open for a bit
+			print(ee)
+			core.pause_and_quit("ERROR: something truly strange and unexpected has occurred, sorry, good luck figuring out what tho")
+
---
+++ __prune_unused_vertexes.py
@@ -0,0 +1,223 @@
+# Nuthouse01 - 03/14/2020 - v3.01
+# This code is free to use and re-distribute, but I cannot be held responsible for damages that it may or may not cause.
+#####################
+
+
+
+import sys
+# first, version check: verify that this is using python3
+# i don't know if it will actually work in 3.4 but i know it will fail in any python2 version
+# actually written/tested with 3.6.6 so guaranteed to work on that or higher
+# between 3.4 and 3.6, who knows
+if sys.version_info < (3, 4):
+	print("Your version of Python is too old to run this script, please update!")
+	print("Your current version = " + sys.version)
+	print("...press ENTER to exit...")
+	input()
+	exit()
+
+# second, wrap custom imports with a try-except to catch it if files are missing
+try:
+	import nuthouse01_core as core
+	import nuthouse01_pmx_parser as pmxlib
+except ImportError as eee:
+	print(eee)
+	print("ERROR: failed to import some of the necessary files, all my scripts must be together in the same folder!")
+	print("...press ENTER to exit...")
+	input()
+	exit()
+	core = pmxlib = None
+
+
+# when debug=True, disable the catchall try-except block. this means the full stack trace gets printed when it crashes,
+# but if launched in a new window it exits immediately so you can't read it.
+DEBUG = False
+
+
+def newval_from_range_map(v, range_map: list):
+	# support both int and list inputs... do basically the same thing, just looped
+	# core idea: walk BACKWARDS along the range_map until i find the start that is CLOSEST BELOW the input v
+	if isinstance(v, int):
+		# idx = len(range_map) - 1
+		# while idx >= len(range_map):
+		for idx in range(len(range_map)-1, -1, -1):
+			if range_map[idx][0] <= v:
+				# return the input value minus the applicable offset
+				return v - range_map[idx][1]
+			# idx -= 1
+		# if it doesnt find a block starting below v, then the offset is 0
+		return v
+	else:
+		# if given a list, the list is ordered so take advantage of that to pick up where the previous item left off
+		# walk backwards along both lists side-by-side
+		retme = []
+		input_idx = len(v) - 1
+		idx = len(range_map) - 1
+		while idx >= 0 and input_idx >= 0:
+			if range_map[idx][0] <= v[input_idx]:
+				# return the input value minus the applicable offset
+				retme.append(v[input_idx] - range_map[idx][1])
+				input_idx -= 1
+			else:
+				idx -= 1
+		if input_idx != -1:
+			# if it finished walking down the range-list before it finished the input-list, all remaining inputs are unchanged
+			retme += reversed(v[0:input_idx+1])
+		retme.reverse()
+		return retme
+	
+
+def main():
+	# TODO: TEST ALL THIS!
+	# print info to explain the purpose of this file
+	print("This script will delete any unused vertices from the model, sometimes causing massive file size improvements.")
+	# print info to explain what inputs it needs
+	print("Inputs: PMX file 'model.pmx'")
+	# print info to explain what outputs it creates
+	print("Outputs: PMX file '[model]_vertprune.pmx'")
+	print("")
+	
+	# prompt PMX name
+	print("Please enter name of PMX model file:")
+	input_filename_pmx = core.prompt_user_filename(".pmx")
+	pmx = pmxlib.read_pmx(input_filename_pmx)
+
+	#############################
+	# ready for logic
+
+	# vertices are referenced in faces, morphs (uv and vertex morphs), and soft bodies (should be handled just for completeness' sake)
+	
+	# find individual vertices to delete
+	#		build set of vertices used in faces
+	#		build set of all vertices (just a range)
+	#		subtract
+	#		convert to sorted list
+	# convert to list of [begin, length]
+	#		iterate over delvertlist, identify contiguous blocks
+	# convert to list of [begin, cumulative size]
+	
+	# build set of USED vertices
+	used_verts = set()
+	for face in pmx[2]:
+		used_verts.add(face[0])
+		used_verts.add(face[1])
+		used_verts.add(face[2])
+	# build set of ALL vertices
+	all_verts = []
+	all_verts.extend(range(len(pmx[1])))
+	all_verts = set(all_verts)
+	# derive set of UNUSED vertices
+	unused_verts = all_verts.difference(used_verts)
+	# convert to ordered list
+	delme_verts = sorted(list(unused_verts))
+	
+	numdeleted = len(delme_verts)
+	prevtotal = len(pmx[1])
+	if numdeleted == 0:
+		print("Nothing to be done")
+		return None
+	
+	# convert from individual vertices to list of ranges, [start-length]
+	# start begins 0, end begins 1
+	delme_range = []
+	start_idx = 0
+	for end_idx in range(1, len(delme_verts)+1):
+		if (end_idx == len(delme_verts)) or (delme_verts[end_idx] != (delme_verts[end_idx-1] + 1)):
+			# if the next vert ID is non-contiguous, or is the end of the list, that defines a breakpoint between ranges
+			# that means that everything from start to end IS contiguous
+			# so save the VALUE of the start, and the LENGTH of the range (which equals the length of the block)
+			delme_range.append([delme_verts[start_idx], end_idx - start_idx])
+			start_idx = end_idx
+	# convert from [start-length] to [start-cumulativelength]
+	for i in range(1, len(delme_range)):
+		delme_range[i][1] += delme_range[i-1][1]
+	
+	print("Detected %d orphan vertices arranged in %d contiguous blocks" % (len(delme_verts), len(delme_range)))
+	
+	# need to update places that reference vertices: faces, morphs, softbody
+	# faces:
+	last_progress = -1
+	for d,face in enumerate(pmx[2]):
+		# display progress printouts
+		if d > last_progress:
+			last_progress += 2000
+			core.print_progress_oneline(d, len(pmx[2]))
+		
+		# vertices in a face are not guaranteed sorted, and changing their order is a Very Bad Idea
+		# therefore they must be handled individually
+		face[0] = newval_from_range_map(face[0], delme_range)
+		face[1] = newval_from_range_map(face[1], delme_range)
+		face[2] = newval_from_range_map(face[2], delme_range)
+	print("Done updating vertex references in faces")
+	
+	# morphs:
+	orphan_vertex_references = 0
+	for d,morph in enumerate(pmx[6]):
+		core.print_progress_oneline(d, len(pmx[6]))
+		# if not a vertex morph or UV morph, skip it
+		if not (3 <= morph[3] <= 7) and morph[3] != 1:
+			continue
+		# first, it is plausible that vertex/uv morphs could reference orphan vertices, so I should check for and delete those
+		i = 0
+		while i < len(morph[4]):
+			# if the vertex being manipulated is in the list of verts being deleted,
+			if morph[4][i][0] in delme_verts:
+				# delete it here too
+				morph[4].pop(i)
+				orphan_vertex_references += 1
+			else:
+				i += 1
+		
+		# assemble the vertices from the morph entries into a list of their own, for more efficient remapping
+		vertlist = [x[0] for x in morph[4]]
+		# remap
+		remappedlist = newval_from_range_map(vertlist, delme_range)
+		# write the remapped values back into where they came from
+		for x, newval in zip(morph[4], remappedlist):
+			x[0] = newval
+	print("Done updating vertex references in morphs")
+
+	# if orphan_vertex_references != 0:
+	# 	print("debug: orphan vertex references removed = ", orphan_vertex_references)
+	
+	# softbody: probably not relevant but eh
+	for soft in pmx[10]:
+		# assemble the vertices from the morph entries into a list of their own, for more efficient remapping
+		anchorlist = [x[1] for x in soft[37]]
+		newanchorlist = newval_from_range_map(anchorlist, delme_range)
+		# write the remapped values back into where they came from
+		for x, newval in zip(soft[37], newanchorlist):
+			x[1] = newval
+		soft[38] = newval_from_range_map(soft[38], delme_range)
+		
+	# now, finally, actually delete the vertices from the vertex list
+	delme_verts.reverse()
+	for f in delme_verts:
+		pmx[1].pop(f)
+	
+	print("Identified and deleted {} / {} = {:.1%} vertices for being unused".format(
+		numdeleted, prevtotal, numdeleted/prevtotal))
+		
+	# write out
+	output_filename_pmx = "%s_vertprune.pmx" % core.get_clean_basename(input_filename_pmx)
+	output_filename_pmx = core.get_unused_file_name(output_filename_pmx)
+	pmxlib.write_pmx(pmx, output_filename_pmx)
+	
+	core.pause_and_quit("Done with everything! Goodbye!")
+	return None
+	
+
+if __name__ == '__main__':
+	print("Nuthouse01 - 03/14/2020 - v3.01")
+	if DEBUG:
+		main()
+	else:
+		try:
+			main()
+		except (KeyboardInterrupt, SystemExit):
+			# this is normal and expected, do nothing and die normally
+			pass
+		except Exception as ee:
+			# if an unexpected error occurs, catch it and print it and call pause_and_quit so the window stays open for a bit
+			print(ee)
+			core.pause_and_quit("ERROR: something truly strange and unexpected has occurred, sorry, good luck figuring out what tho")
