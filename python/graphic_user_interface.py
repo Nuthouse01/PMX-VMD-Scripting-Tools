9e65e1fbffd606ba566e713c2a5996b0ec986cb2
Brian Henson <brian.henson1@yahoo.com>
2020-04-02 12:15:29 -0700

    Update graphic_user_interface.py

--- python/graphic_user_interface.py
+++ python/graphic_user_interface.py
@@ -88,35 +88,56 @@ import tkinter.filedialog as fdg
 import nuthouse01_core as core
 import nuthouse01_pmx_parser as pmxlib
 from os import path
-import time
-import sys
 import pmx_overall_cleanup
 import copy
 
 
-# d = {}
-# d[4] = "asdf"
-# try:
-# 	print(d[2])
-# except Exception as e:
-# 	print(e)
-# 	print(sys.exc_info())
+
+
+# # a popupwindow to replace the stock "input()" function
+# def gui_input(prompt=''):
+# 	win = tk.Toplevel()
+#
+# 	# normally when x button is pressed, it calls "destroy". this replaces that with "quit", to break mainloop
+# 	# that way the x button resumes executing below win.mainloop and the script continues
+# 	win.protocol("WM_DELETE_WINDOW", win.quit)
+#
+# 	label= tk.Label(win, text=prompt)
+# 	label.pack()
+#
+# 	userinput= tk.StringVar(win)
+# 	entry= tk.Entry(win, textvariable=userinput)
+# 	entry.pack()
+#
+# 	# pressing the button should stop the mainloop
+# 	button= tk.Button(win, text="ok", command=win.quit)
+# 	button.pack()
+#
+# 	# block execution until the user presses the OK button
+# 	win.mainloop()
+#
+# 	# mainloop has ended. Read the value of the Entry, then destroy the GUI.
+# 	userinput= userinput.get()
+# 	win.destroy()
+#
+# 	return userinput
+
 
 
 
 
 class Application(tk.Frame):
-	def __init__(self, master):
+	def __init__(self, master, help_func, run_func, writeout_func, UIconfig):
 		tk.Frame.__init__(self, master)
 		
 		###############################################
 		# first, handle input parameters
 		# parameters: help function, payload function, writeout function, what kind of input buttons (pmx, vmd, txt)
 		
-		self.help_func =     pmx_overall_cleanup.showallhelp
-		self.run_func =      pmx_overall_cleanup.pmx_overall_cleanup
-		self.writeout_func = pmx_overall_cleanup.end
-		self.UIconfig = ("pmx",)
+		self.help_func =     help_func
+		self.run_func =      run_func
+		self.writeout_func = writeout_func
+		self.UIconfig =      UIconfig
 		# UI config: do you want a pmx button? do you want a vmd button? do you want both? do you want a txt button?
 		
 		###############################################
@@ -173,10 +194,6 @@ class Application(tk.Frame):
 		self.debug_check = tk.Checkbutton(self.always_frame, text="show extra info", variable=self.debug_check_var)
 		self.debug_check.pack(side=tk.RIGHT, padx=10, pady=10)
 		
-		# self.debug_check.
-		# # frame = tk.Frame(master, bg='brown')
-		# frame = tk.Frame(master)
-		# frame.pack(fill='both', expand='yes')
 		###############################################
 		# fourth, build the "scrolledtext" object to serve as my output terminal
 		self.edit_space = tkst.ScrolledText(
@@ -193,14 +210,7 @@ class Application(tk.Frame):
 		core.MY_PRINT_FUNC = self.my_write
 		
 		core.MY_PRINT_FUNC("Nuthouse01 - 03/30/2020 - v3.51")
-		
-		mytext = '''\
-		Man who drive like hell, bound to get there.
-		Man who run in front of car, get tired.
-		Man who run behind car, get exhausted. 全ての親
-		The Internet: where men are men, women are men, and children are FBI agents.
-		'''
-		core.MY_PRINT_FUNC(mytext)
+		core.MY_PRINT_FUNC("Begin by loading an input file, then click 'Run'")
 		
 		# done with init
 		return
@@ -301,171 +311,17 @@ class Application(tk.Frame):
 		self.run_butt.configure(state='normal')
 		return
 
-def launch_gui():
+def launch_gui(title, help_func, run_func, writeout_func, UIconfig):
 	root = tk.Tk()
-	root.title('ScrolledText test')
-	app = Application(root)
+	root.title(title)
+	app = Application(root, help_func, run_func, writeout_func, UIconfig)
 	app.mainloop()
 
 
 if __name__ == '__main__':
-	launch_gui()
-
-'''
-# this is the function to run when something changes and its time to send a message to ROS or inmoov
-# this is a function that takes a single string
-self.on_change_callback = on_change_callback
-
-self.mode = 0  # mode 0=left, 1=center, 2=right
-
-self.maxtablistlength = 0  # the most servos in a tab
-for namelist in names_all:
-	self.maxtablistlength = max(self.maxtablistlength, len(namelist))
-# current_values is initiallized with all zeros, in the same shape as names_all
-# used to determine which changed when something changed, also to know what to display when loading a tab
-self.current_values = []
-for p in names_all:
-	self.current_values.append([0] * len(p))
-# current_checkbox_states is same as current_values but for the checkboxes_vars
-# disabled servos begin with this checkbox off
-self.pose_init = {}
-self.current_checkbox_states = []
-self.current_onoff_states = []
-for p in names_all:
-	boxes = []
-	onoff = []
-	for n in p:
-		s = my_inmoov.find_servo_by_name(n)
-		self.pose_init[n] = s.default_angle
-		boxes.append(int(not s.disabled))
-		onoff.append(0)
-	self.current_checkbox_states.append(boxes)
-	self.current_onoff_states.append(onoff)
-
-# applying an "empty pose" doesn't change any sliders, but it unchecks all checkboxes
-self.pose_off = {}
-
-#####################################
-# begin GUI setup
-# structure:
-# one frame for all buttons stacked vertically with one frame for the scrollbar stuff
-# inside buttons frame, everything packed horizontally
-# buttons grouped with visible borders to show things that are related:
-# left/right/center buttons inside a frame with a visible border
-# init/off buttons inside a frame with a visible border
-# name/save field/button inside a frame with a visible border
-
-# buttons for control & stuff
-self.frame_biggroup1 = tk.Frame(master)
-self.frame_biggroup1.pack(side=tk.TOP)
-
-# self.frame_tab_buttons = tk.Frame(self.frame_biggroup1, highlightbackground="black", highlightthickness=1)
-self.frame_tab_buttons = tk.Frame(self.frame_biggroup1, relief=tk.RAISED, borderwidth=1)
-self.frame_tab_buttons.pack(side=tk.LEFT, padx=10, pady=10)
-self.butt_left = tk.Button(self.frame_tab_buttons, text="Left", width=button_width,
-						   command=lambda: self.change_tab(0))
-self.butt_left.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-self.butt_center = tk.Button(self.frame_tab_buttons, text="Center", width=button_width,
-							 command=lambda: self.change_tab(1))
-self.butt_center.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-self.butt_right = tk.Button(self.frame_tab_buttons, text="Right", width=button_width,
-							command=lambda: self.change_tab(2))
-self.butt_right.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-
-self.frame_command_buttons = tk.Frame(self.frame_biggroup1, relief=tk.RAISED, borderwidth=1)
-self.frame_command_buttons.pack(side=tk.LEFT, pady=10)
-self.butt_init = tk.Button(self.frame_command_buttons, text="INIT", width=button_width,
-						   command=self.allservos_init)
-self.butt_init.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-self.butt_off = tk.Button(self.frame_command_buttons, text="OFF", width=button_width,
-						  command=self.allservos_off)
-self.butt_off.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-
-self.frame_posesave_widgets = tk.Frame(self.frame_biggroup1, relief=tk.RAISED, borderwidth=1)
-self.frame_posesave_widgets.pack(side=tk.LEFT, padx=10, pady=10)
-self.name_entry = tk.Entry(self.frame_posesave_widgets, width=textentry_width)
-self.name_entry.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-self.butt_save = tk.Button(self.frame_posesave_widgets, text="Save", width=button_width,
-						   command=self.save_values)
-self.butt_save.pack(side=tk.LEFT, pady=button_pady)
-self.butt_load = tk.Button(self.frame_posesave_widgets, text="Load", width=button_width,
-						   command=self.load_values)
-self.butt_load.pack(side=tk.LEFT, padx=button_padx, pady=button_pady)
-
-# the default color always looks the same but has different names on different platforms
-self.defaultcolor_buttonback = self.butt_off.cget("background")
-print(self.defaultcolor_buttonback)
-
-# Scale.config(state=tk.DISABLED)
-# Scale.config(state=tk.NORMAL)
-# Label.config(text="asdf")
-
-###########################################################################
-# build & fill the scrollbar region
-
-# frame that contains everything that isn't the above buttons
-self.frame_biggroup2 = tk.Frame(master)
-self.frame_biggroup2.pack(fill=tk.BOTH, expand=True)
-
-# canvas holds a frame widget
-canvas_width = int(slider_width + (7.5 * label_width) + 23)  # set the default and minimum width
-self.canvas = tk.Canvas(self.frame_biggroup2, height=canvas_height, width=canvas_width)
-self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-# scrollbar
-# change the canvas when the scrollbar is scrolled
-self.scrollbar = tk.Scrollbar(self.frame_biggroup2, command=self.canvas.yview)
-self.scrollbar.pack(side=tk.LEFT, fill='y')
-# set the scrollbar when something changes the canvas (window resizing)
-self.canvas.configure(yscrollcommand=self.scrollbar.set)
-# not totally sure what this does, gets called when the canvas is resized (window resizing)
-self.canvas.bind('<Configure>', self.update_canvas_size)
-
-# --- put frame in canvas ---
-self.frame_servolist = tk.Frame(self.canvas)
-self.canvas.create_window((0, 0), window=self.frame_servolist, anchor='nw')
-
-# --- add widgets in frame ---
-self.sliders = []
-self.labels = []
-self.checkboxes = []
-self.checkboxes_vars = []
-for i in range(self.maxtablistlength):
-	w = tk.Label(self.frame_servolist, text="Hello Tkinter!" + str(i), width=label_width)
-	w.grid(row=i, column=0)
-	self.labels.append(w)
-
-	s = tk.Scale(self.frame_servolist, from_=0, to=200, length=slider_width, tickinterval=30,
-				 orient=tk.HORIZONTAL, command=self.get_changed_sliders)
-	s.grid(row=i, column=1)
-	self.sliders.append(s)
-
-	var = tk.IntVar()
-	c = tk.Checkbutton(self.frame_servolist, variable=var, text="", command=self.get_changed_checkbox)
-	# var.set(1) # turn the button on by default
-	c.grid(row=i, column=2)
-	self.checkboxes.append(c)
-	self.checkboxes_vars.append(var)
-
-# w.get() to return current slider val
-# w.set(x) to set initial value
-# resolution: default 1, set lower for floatingpoint
-# command: callback, gets value as only arg
-
-# the default color always looks the same but has different names on different platforms
-self.defaultcolor_font = self.labels[0].cget("fg")
-print(self.defaultcolor_font)
-
-self.change_tab(0)
-# DONE WITH GUI INIT
-pass
-'''
-
-# root > app(frame)? > everything
-
-
-
-# optiona info
-# help(tkst.ScrolledText)
-
+	launch_gui("PMX Overall Cleanup",
+			   pmx_overall_cleanup.showallhelp,
+			   pmx_overall_cleanup.pmx_overall_cleanup,
+			   pmx_overall_cleanup.end,
+			   ("pmx",))
 
