e5918c78d2fee981cf0abe3525ce0ea91b14da71
Brian Henson <brian.henson1@yahoo.com>
2020-08-15 11:47:11 -0700

    add half-katakana to full-katakana piecewise translation dict
    don't have any plans to actually use it tho

--- python/_translation_tools.py
+++ python/_translation_tools.py
@@ -16,6 +16,127 @@
 import re
 from typing import List, Union, Tuple
 
+
+
+# dictionary for translating halfwitdth katakana to fullwidth katakana
+# i have no plans to actually use this but now it exists
+half_to_full_dict = {
+# dot
+'\uff65':	'\u30fb',
+# prolong
+'\uff70':	'\u30fc',
+# halfwidth "
+# "		\uff9e
+# halfwidth deg *
+# *		\uff9f
+# aeiou big+small
+'\uff67':	'\u30a1',
+'\uff68':	'\u30a3',
+'\uff69':	'\u30a5',
+'\uff6a':	'\u30a7',
+'\uff6b':	'\u30a9',
+'\uff71':	'\u30a2',
+'\uff72':	'\u30a4',
+'\uff73':	'\u30a6',
+'\uff74':	'\u30a8',
+'\uff75':	'\u30aa',
+# ya yu yo tu small
+'\uff6c':	'\u30e3',
+'\uff6d':	'\u30e5',
+'\uff6e':	'\u30e7',
+'\uff6f':	'\u30c3',
+# ya yu yo big
+'\uff94':	'\u30e4',
+'\uff95':	'\u30e6',
+'\uff96':	'\u30e8',
+# pattern: x, x+"
+'\uff76':		'\u30ab',
+'\uff76\uff9e':	'\u30ac',
+'\uff77':		'\u30ad',
+'\uff77\uff9e':	'\u30ae',
+'\uff78':		'\u30af',
+'\uff78\uff9e':	'\u30b0',
+'\uff79':		'\u30b1',
+'\uff79\uff9e':	'\u30b2',
+'\uff7a':		'\u30b3',
+'\uff7a\uff9e':	'\u30b4',
+'\uff7b':		'\u30b5',
+'\uff7b\uff9e':	'\u30b6',
+'\uff7c':		'\u30b7',
+'\uff7c\uff9e':	'\u30b8',
+'\uff7d':		'\u30b9',
+'\uff7d\uff9e':	'\u30ba',
+'\uff7e':		'\u30bb',
+'\uff7e\uff9e':	'\u30bc',
+'\uff7f':		'\u30bd',
+'\uff7f\uff9e':	'\u30be',
+'\uff80':		'\u30bf',
+'\uff80\uff9e':	'\u30b0',
+'\uff81':		'\u30c1',
+'\uff81\uff9e':	'\u30c2',
+'\uff82':		'\u30c4',
+'\uff82\uff9e':	'\u30c5',
+'\uff83':		'\u30c6',
+'\uff83\uff9e':	'\u30c7',
+'\uff84':		'\u30c8',
+'\uff84\uff9e':	'\u30c9',
+# x, x", x*
+'\uff8a':		'\u30cf',
+'\uff8a\uff9e':	'\u30d0',
+'\uff8a\uff9f':	'\u30d1',
+'\uff8b':		'\u30d2',
+'\uff8b\uff9e':	'\u30d3',
+'\uff8b\uff9f':	'\u30d4',
+'\uff8c':		'\u30d5',
+'\uff8c\uff9e':	'\u30d6',
+'\uff8c\uff9f':	'\u30d7',
+'\uff8d':		'\u30d8',
+'\uff8d\uff9e':	'\u30d9',
+'\uff8d\uff9f':	'\u30da',
+'\uff8e':		'\u30db',
+'\uff8e\uff9e':	'\u30dc',
+'\uff8e\uff9f':	'\u30dd',
+# n sounds
+'\uff85':	'\u30ca',
+'\uff86':	'\u30cb',
+'\uff87':	'\u30cc',
+'\uff88':	'\u30cd',
+'\uff89':	'\u30ce',
+# m sounds
+'\uff8f':	'\u30de',
+'\uff90':	'\u30df',
+'\uff91':	'\u30e0',
+'\uff92':	'\u30e1',
+'\uff93':	'\u30e2',
+# r sounds
+'\uff97':	'\u30e9',
+'\uff98':	'\u30ea',
+'\uff99':	'\u30eb',
+'\uff9a':	'\u30ec',
+'\uff9b':	'\u30ed',
+# wa,wo,n,vu
+'\uff9c':		'\u30ef',
+'\uff9c\uff9e':	'\u30f7',
+'\uff66':		'\u30f2',
+'\uff66\uff9e':	'\u30fa',
+'\uff9d':		'\u30f3',
+'\uff73\uff9e':	'\u30f4',
+#### fullwidth wi,we have no halfwidth counterpart at all
+# 30f0	30f0
+# 30f1	30f1
+'\u30f0\uff9e':	'\u30f8',
+'\u30f1\uff9e':	'\u30f9',
+}
+
+#### wa, ka, ke have fullwidth small ver but only halfwidth counterpart is big ver
+#### only useful when going full -> half
+# ?ff9c	30ee
+# ?ff76	30f5
+# ?ff79	30f6
+
+
+
+
 # this dict is added to both "words" and "morphs"... just in one place so I can keep thing straight
 symbols_dict = {
 "╱": "/",  # x2571 "box drawing" section.
@@ -108,6 +229,7 @@ morph_dict = {
 "い": "i",
 "う": "u",
 "え": "e",
+"えー": "eeeeeh",  # long "e" sound
 "お": "o",
 "ワ": "wa",
 "わ": "wa",
