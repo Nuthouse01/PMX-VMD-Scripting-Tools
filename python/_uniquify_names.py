ba556f3f87c83271be3516f0a0e3ca9bf8b39d60
Brian Henson <brian.henson1@yahoo.com>
2020-08-19 10:29:29 -0700

    WIP: last 2 sub-scripts

--- python/_uniquify_names.py
+++ python/_uniquify_names.py
@@ -6,17 +6,19 @@
 try:
 	from . import nuthouse01_core as core
 	from . import nuthouse01_pmx_parser as pmxlib
+	from . import nuthouse01_pmx_struct as pmxstruct
 except ImportError as eee:
 	try:
 		import nuthouse01_core as core
 		import nuthouse01_pmx_parser as pmxlib
+		import nuthouse01_pmx_struct as pmxstruct
 	except ImportError as eee:
 		print(eee.__class__.__name__, eee)
 		print("ERROR: failed to import some of the necessary files, all my scripts must be together in the same folder!")
 		print("...press ENTER to exit...")
 		input()
 		exit()
-		core = pmxlib = None
+		core = pmxlib = pmxstruct = None
 
 
 # when debug=True, disable the catchall try-except block. this means the full stack trace gets printed when it crashes,
@@ -68,7 +70,7 @@ def showprompt():
 	pmx = pmxlib.read_pmx(input_filename_pmx, moreinfo=True)
 	return pmx, input_filename_pmx
 
-def uniquify_names(pmx, moreinfo=False):
+def uniquify_names(pmx: pmxstruct.Pmx, moreinfo=False):
 	
 	# just uniquify the names
 	# return counts of how many en/jp from each category were changed
@@ -76,13 +78,14 @@ def uniquify_names(pmx, moreinfo=False):
 	counts = [0] * 8
 	counts_labels = ["material_JP","bone_JP","morph_JP","dispframe_JP","material_EN","bone_EN","morph_EN","dispframe_EN"]
 	
-	for cat_id in range(4, 8):
-		category = pmx[cat_id]
+	cat_id_list = list(range(4,8))
+	category_list = [pmx.materials, pmx.bones, pmx.morphs, pmx.frames]
+	for cat_id, category in zip(cat_id_list, category_list):
 		used_en_names = set()
 		used_jp_names = set()
 		for i, item in enumerate(category):
-			jp_name = item[0]
-			en_name = item[1]
+			jp_name = item.name_jp
+			en_name = item.name_en
 			# first, uniquify the jp name
 			if jp_name != "" or ALSO_UNIQUIFY_NULL_NAMES:
 				new_jp_name = uniquify_one_category(used_jp_names, jp_name)
@@ -90,7 +93,7 @@ def uniquify_names(pmx, moreinfo=False):
 				if new_jp_name != jp_name:
 					if moreinfo: core.MY_PRINT_FUNC("%s: #%d    %s --> %s" % (counts_labels[cat_id - 4], i, jp_name, new_jp_name))
 					# count & store into the structure
-					item[0] = new_jp_name
+					item.name_jp = new_jp_name
 					counts[cat_id - 4] += 1
 			# second, uniquify the en name
 			if en_name != "" or ALSO_UNIQUIFY_NULL_NAMES:
@@ -99,7 +102,7 @@ def uniquify_names(pmx, moreinfo=False):
 				if new_en_name != en_name:
 					if moreinfo: core.MY_PRINT_FUNC("%s: #%d    %s --> %s" % (counts_labels[cat_id], i, en_name, new_en_name))
 					# count & store into the structure
-					item[1] = new_en_name
+					item.name_en = new_en_name
 					counts[cat_id] += 1
 	
 	counts_dict = {x:y for x,y in zip(counts_labels, counts) if y != 0}
