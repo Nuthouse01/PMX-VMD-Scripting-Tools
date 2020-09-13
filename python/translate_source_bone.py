c75225ee0e2ed2d8b841dffd6afefa8b682dc911
khang <khanghugo123@gmail.com>
2020-09-13 08:42:17 -0400

    update dictionary format

--- python/translate_source_bone.py
+++ python/translate_source_bone.py
@@ -6,12 +6,14 @@ try:
     from . import nuthouse01_pmx_struct as pmxstruct
     from . import file_sort_textures
     from ._prune_unused_vertices import newval_from_range_map, delme_list_to_rangemap
+    from ._prune_unused_bones import insert_single_bone
 except ImportError as eee:
     try:
         import nuthouse01_core as core
         import nuthouse01_pmx_parser as pmxlib
         import nuthouse01_pmx_struct as pmxstruct
         import file_sort_textures
+        from _prune_unused_bones import insert_single_bone
         from _prune_unused_vertices import newval_from_range_map, delme_list_to_rangemap
     except ImportError as eee:
         print(eee.__class__.__name__, eee)
@@ -51,137 +53,70 @@ Will result in object/list error where struct does not pass on an object attribu
 
 # This takes PMX output from Crowbar
 # New BIP
-new_finger_dict = {
-    "ValveBiped.Bip01_R_Finger4": "右小指１",
-    "ValveBiped.Bip01_R_Finger41": "右小指２",
-    "ValveBiped.Bip01_R_Finger42": "右小指３",
-    "ValveBiped.Bip01_R_Finger3": "右薬指１",
-    "ValveBiped.Bip01_R_Finger31": "右薬指２",
-    "ValveBiped.Bip01_R_Finger32": "右薬指３",
-    "ValveBiped.Bip01_R_Finger2": "右中指１",
-    "ValveBiped.Bip01_R_Finger21": "右中指２",
-    "ValveBiped.Bip01_R_Finger22": "右中指３",
-    "ValveBiped.Bip01_R_Finger1": "右人指１",
-    "ValveBiped.Bip01_R_Finger11": "右人指２",
-    "ValveBiped.Bip01_R_Finger12": "右人指３",
-    "ValveBiped.Bip01_R_Finger0": "右親指１",
-    "ValveBiped.Bip01_R_Finger01": "右親指２",
-    "ValveBiped.Bip01_R_Finger02": "右親指３",  # no bone for the second joint here
+finger_dict = {
+    ["ValveBiped.Bip01_R_Finger4", "bip_pinky_0_R"]: "右小指１",
+    ["ValveBiped.Bip01_R_Finger41", "bip_pinky_1_R"]: "右小指２",
+    ["ValveBiped.Bip01_R_Finger42", "bip_pinky_2_R"]: "右小指３",
+    ["ValveBiped.Bip01_R_Finger3", "bip_ring_0_R"]: "右薬指１",
+    ["ValveBiped.Bip01_R_Finger31", "bip_ring_1_R"]: "右薬指２",
+    ["ValveBiped.Bip01_R_Finger32", "bip_ring_2_R"]: "右薬指３",
+    ["ValveBiped.Bip01_R_Finger2", "bip_middle_0_R"]: "右中指１",
+    ["ValveBiped.Bip01_R_Finger21", "bip_middle_1_R"]: "右中指２",
+    ["ValveBiped.Bip01_R_Finger22", "bip_middle_2_R"]: "右中指３",
+    ["ValveBiped.Bip01_R_Finger1", "bip_index_0_R"]: "右人指１",
+    ["ValveBiped.Bip01_R_Finger11", "bip_index_1_R"]: "右人指２",
+    ["ValveBiped.Bip01_R_Finger12", "bip_index_2_R"]: "右人指３",
+    ["ValveBiped.Bip01_R_Finger0", "bip_thumb_0_R"]: "右親指１",
+    ["ValveBiped.Bip01_R_Finger01", "bip_thumb_1_R"]: "右親指２",
+    ["ValveBiped.Bip01_R_Finger02", "bip_thumb_2_R"]: "右親指３",  # no bone for the second joint here but anyway
 
-    "ValveBiped.Bip01_L_Finger4": "左小指１",
-    "ValveBiped.Bip01_L_Finger41": "左小指２",
-    "ValveBiped.Bip01_L_Finger42": "左小指３",
-    "ValveBiped.Bip01_L_Finger3": "左薬指１",
-    "ValveBiped.Bip01_L_Finger31": "左薬指２",
-    "ValveBiped.Bip01_L_Finger32": "左薬指３",
-    "ValveBiped.Bip01_L_Finger2": "左中指１",
-    "ValveBiped.Bip01_L_Finger21": "左中指２",
-    "ValveBiped.Bip01_L_Finger22": "左中指３",
-    "ValveBiped.Bip01_L_Finger1": "左人指１",
-    "ValveBiped.Bip01_L_Finger11": "左人指２",
-    "ValveBiped.Bip01_L_Finger12": "左人指３",
-    "ValveBiped.Bip01_L_Finger0": "左親指１",
-    "ValveBiped.Bip01_L_Finger01": "左親指２",
-    "ValveBiped.Bip01_L_Finger02": "左親指３"
+    ["ValveBiped.Bip01_L_Finger4", "bip_pinky_0_L"]: "左小指１",
+    ["ValveBiped.Bip01_L_Finger41", "bip_pinky_1_L"]: "左小指２",
+    ["ValveBiped.Bip01_L_Finger42", "bip_pinky_2_L"]: "左小指３",
+    ["ValveBiped.Bip01_L_Finger3", "bip_ring_0_L"]: "左薬指１",
+    ["ValveBiped.Bip01_L_Finger31", "bip_ring_1_L"]: "左薬指２",
+    ["ValveBiped.Bip01_L_Finger32", "bip_ring_2_L"]: "左薬指３",
+    ["ValveBiped.Bip01_L_Finger2", "bip_middle_0_L"]: "左中指１",
+    ["ValveBiped.Bip01_L_Finger21", "bip_middle_1_L"]: "左中指２",
+    ["ValveBiped.Bip01_L_Finger22", "bip_middle_2_L"]: "左中指３",
+    ["ValveBiped.Bip01_L_Finger1", "bip_index_0_L"]: "左人指１",
+    ["ValveBiped.Bip01_L_Finger11", "bip_index_1_L"]: "左人指２",
+    ["ValveBiped.Bip01_L_Finger12", "bip_index_2_L"]: "左人指３",
+    ["ValveBiped.Bip01_L_Finger0", "bip_thumb_0_L"]: "左親指１",
+    ["ValveBiped.Bip01_L_Finger01", "bip_thumb_1_L"]: "左親指２",
+    ["ValveBiped.Bip01_L_Finger02", "bip_thumb_2_L"]: "左親指３"
 }
-new_arm_dict = {
-    "ValveBiped.Bip01_R_Clavicle": "右肩",
-    "ValveBiped.Bip01_R_UpperArm": "右腕",
-    "ValveBiped.Bip01_R_Forearm": "右ひじ",
-    "ValveBiped.Bip01_R_Hand": "右手捩",
+arm_dict = {
+    ["ValveBiped.Bip01_R_Clavicle", "bip_collar_R"]: "右肩",
+    ["ValveBiped.Bip01_R_UpperArm", "bip_upperArm_R"]: "右腕",
+    ["ValveBiped.Bip01_R_Forearm", "bip_lowerArm_R"]: "右ひじ",
+    ["ValveBiped.Bip01_R_Hand", "bip_hand_R"]: "右手捩",
 
-    "ValveBiped.Bip01_L_Clavicle": "左肩",
-    "ValveBiped.Bip01_L_UpperArm": "左腕",
-    "ValveBiped.Bip01_L_Forearm": "左ひじ",
-    "ValveBiped.Bip01_L_Hand": "左手捩"
+    ["ValveBiped.Bip01_L_Clavicle", "bip_collar_L"]: "左肩",
+    ["ValveBiped.Bip01_L_UpperArm", "bip_upperArm_L"]: "左腕",
+    ["ValveBiped.Bip01_L_Forearm", "bip_lowerArm_L"]: "左ひじ",
+    ["ValveBiped.Bip01_L_Hand", "bip_hand_L"]: "左手捩"
 }
-new_leg_dict = {
-    "ValveBiped.Bip01_R_Thigh": "右足",
-    "ValveBiped.Bip01_R_Calf": "右ひざ",
-    "ValveBiped.Bip01_R_Foot": "右足首",
-    "ValveBiped.Bip01_R_Toe0": "右つま先",
+leg_dict = {
+    ["ValveBiped.Bip01_R_Thigh", "bip_hip_R"]: "右足",
+    ["ValveBiped.Bip01_R_Calf", "bip_knee_R"]: "右ひざ",
+    ["ValveBiped.Bip01_R_Foot", "bip_foot_R"]: "右足首",
+    ["ValveBiped.Bip01_R_Toe0", "bip_toe_R"]: "右つま先",
 
-    "ValveBiped.Bip01_L_Thigh": "左足",
-    "ValveBiped.Bip01_L_Calf": "左ひざ",
-    "ValveBiped.Bip01_L_Foot": "左足首",
-    "ValveBiped.Bip01_L_Toe0": "左つま先"
+    ["ValveBiped.Bip01_L_Thigh", "bip_hip_L"]: "左足",
+    ["ValveBiped.Bip01_L_Calf", "bip_knee_L"]: "左ひざ",
+    ["ValveBiped.Bip01_L_Foot", "bip_foot_L"]: "左足首",
+    ["ValveBiped.Bip01_L_Toe0", "bip_toe_L"]: "左つま先"
 }
-new_body_dict = {
-    "ValveBiped.Bip01_Pelvis": "下半身",
-    "ValveBiped.Bip01_Spine": "下半身",
-    "ValveBiped.Bip01_Spine1": "上半身",
-    "ValveBiped.Bip01_Spine2": "上半身2",
-    "ValveBiped.Bip01_Spine4": "首",  # this is at the base of the neck, we can combine it
-    "ValveBiped.Bip01_Neck1": "首",
-    "ValveBiped.Bip01_Head1": "頭"
+body_dict = {
+    ["ValveBiped.Bip01_Pelvis", "bip_pelvis"]: "下半身",
+    ["ValveBiped.Bip01_Spine", "bip_spine_0"]: "下半身",
+    ["ValveBiped.Bip01_Spine1", "bip_spine_1"]: "上半身",
+    ["ValveBiped.Bip01_Spine2", "bip_spine_2"]: "上半身2",
+    ["ValveBiped.Bip01_Spine4", "bip_spine_3"]: "首",  # this is at the base of the neck, we can combine it
+    ["ValveBiped.Bip01_Neck1", "bip_neck"]: "首",
+    ["ValveBiped.Bip01_Head1", "bip_head"]: "頭"
 }
-
-# Old BIP
-old_finger_dict = {
-    "bip_index_0_R": "右親指１",
-    "bip_index_1_R": "右親指２",
-    "bip_index_2_R": "右親指３",
-    "bip_thumb_0_R": "右人指１",
-    "bip_thumb_1_R": "右人指２",
-    "bip_thumb_2_R": "右人指３",
-    "bip_middle_0_R": "右中指１",
-    "bip_middle_1_R": "右中指２",
-    "bip_middle_2_R": "右中指３",
-    "bip_ring_0_R": "右薬指１",
-    "bip_ring_1_R": "右薬指２",
-    "bip_ring_2_R": "右薬指３",
-    "bip_pinky_0_R": "右小指１",
-    "bip_pinky_1_R": "右小指２",
-    "bip_pinky_2_R": "右小指３",
-
-    "bip_index_0_L": "左親指１",
-    "bip_index_1_L": "左親指２",
-    "bip_index_2_L": "左親指３",
-    "bip_thumb_0_L": "左人指１",
-    "bip_thumb_1_L": "左人指２",
-    "bip_thumb_2_L": "左人指３",
-    "bip_middle_0_L": "左中指１",
-    "bip_middle_1_L": "左中指２",
-    "bip_middle_2_L": "左中指３",
-    "bip_ring_0_L": "左薬指１",
-    "bip_ring_1_L": "左薬指２",
-    "bip_ring_2_L": "左薬指３",
-    "bip_pinky_0_L": "左小指１",
-    "bip_pinky_1_L": "左小指２",
-    "bip_pinky_2_L": "左小指３"
-}
-old_arm_dict = {
-    "bip_collar_R": "右肩",
-    "bip_upperArm_R": "右腕",
-    "bip_lowerArm_R": "右ひじ",
-    "bip_hand_R": "右手捩",
-
-    "bip_collar_L": "左肩",
-    "bip_upperArm_L": "左腕",
-    "bip_lowerArm_L": "左ひじ",
-    "bip_hand_L": "左手捩"
-}
-old_leg_dict = {
-    "bip_hip_R": "右足",
-    "bip_knee_R": "右ひざ",
-    "bip_foot_R": "右足首",
-    "bip_toe_R": "右つま先",
-
-    "bip_hip_L": "左足",
-    "bip_knee_L": "左ひざ",
-    "bip_foot_L": "左足首",
-    "bip_toe_L": "左つま先"
-}
-old_body_dict = {
-    "bip_pelvis": "下半身",
-    "bip_spine_0": "下半身",
-    "bip_spine_1": "上半身",
-    "bip_spine_2": "上半身2",
-    "bip_spine_3": "首",
-    "bip_neck": "首",
-    "bip_head": "頭"
-}
-
 
 # base order: 上半身, 下半身, 腰, グルーブ, センター, 全ての親
 # the rest of the work should be done in pmxeditor instead, jsut one click away
@@ -202,27 +137,19 @@ def main():
 
     # since there is an update to Valve Bip tools (I guess?), there is different bone names: the old and new one
     # only prefixes are changed along with order, thus there is a little bit scripting here to find the last leg
-    big_dict: dict
+    big_dict: dict = {**body_dict, **leg_dict, **arm_dict, **finger_dict}
+
+    # for some reasons, we can find item in a list with a boolean index, it is unused anyway
     is_old: bool = False
 
-    if "bip_" in retme.bones[0].name_jp:
-        big_dict = {**old_body_dict,
-                    **old_arm_dict,
-                    **old_leg_dict,
-                    **old_finger_dict}
+    old_bip_key = "bip_"
+    # just to make sure with many instances
+    if old_bip_key in retme.bones[0].name_jp or old_bip_key in retme.bones[1].name_jp or old_bip_key in retme.bones[2].name_jp:
         is_old = True
-
-    else:
-        big_dict = {**new_body_dict,
-                    **new_arm_dict,
-                    **new_leg_dict,
-                    **new_finger_dict}
 
     # checking for last leg item so the code could be scalable
     last_leg_item: int
     last_leg_name: str
-    last_leg_name_cmp_r: str = ""
-    last_leg_name_cmp_l: str = ""
 
     r_l_index: int = 0
     r_k_index: int = 0
@@ -233,45 +160,37 @@ def main():
     l_a_index: int = 0
     l_t_index: int = 0
 
+    last_bone_index = len(retme.bones) - 1
+
     # lol this is a mess but it works just fine okay
     for key in big_dict:
         for index, i in enumerate(retme.bones):
             # usually, the toes are the last parts of the legs, from there, we can interject the IK bones
-            if i.name_jp == "bip_toe_R" or i.name_jp == "ValveBiped.Bip01_R_Toe0":
+            if i.name_jp in ["ValveBiped.Bip01_R_Toe0", "bip_toe_R"]:
                 r_t_index = index
-                # last_leg_name_cmp_r = i.name_jp
-            elif i.name_jp == "bip_toe_L" or i.name_jp == "ValveBiped.Bip01_L_Toe0":
+            elif i.name_jp in ["ValveBiped.Bip01_L_Toe0", "bip_toe_L"]:
                 l_t_index = index
-                # last_leg_name_cmp_l = i.name_jp
-            # without this, the pelvis will start as "green"
-            elif i.name_jp == "ValveBiped.Bip01_Pelvis" or i.name_jp == "bip_pelvis":
+
+            # without this, the pelvis will show as "green"
+            elif i.name_jp in ["ValveBiped.Bip01_Pelvis", "bip_pelvis"]:
                 retme.bones[index].has_translate = False
 
-            elif i.name_jp == "ValveBiped.Bip01_R_Foot" or i.name_jp == "bip_foot_R":
+            elif i.name_jp in ["ValveBiped.Bip01_R_Foot", "bip_foot_R"]:
                 r_a_index = index
-            elif i.name_jp == "ValveBiped.Bip01_L_Foot" or i.name_jp == "bip_foot_L":
+            elif i.name_jp in ["ValveBiped.Bip01_L_Foot", "bip_foot_L"]:
                 l_a_index = index
-            elif i.name_jp == "ValveBiped.Bip01_R_Calf" or i.name_jp == "bip_knee_R":
+            elif i.name_jp in ["ValveBiped.Bip01_R_Calf", "bip_knee_R"]:
                 r_k_index = index
-            elif i.name_jp == "ValveBiped.Bip01_L_Calf" or i.name_jp == "bip_knee_L":
+            elif i.name_jp in ["ValveBiped.Bip01_L_Calf", "bip_knee_L"]:
                 l_k_index = index
-            elif i.name_jp == "ValveBiped.Bip01_R_Thigh" or i.name_jp == "bip_hip_R":
+            elif i.name_jp in ["ValveBiped.Bip01_R_Thigh", "bip_hip_R"]:
                 r_l_index = index
-            elif i.name_jp == "ValveBiped.Bip01_L_Thigh" or i.name_jp == "bip_hip_L":
+            elif i.name_jp in ["ValveBiped.Bip01_L_Thigh", "bip_hip_L"]:
                 l_l_index = index
 
             # the part that replaces texts
-            if i.name_jp == key:
+            if i.name_jp in key:
                 retme.bones[index].name_jp = big_dict[key]
-
-    last_bone = len(retme.bones) - 1
-    # if r_t_index > l_t_index:
-    #     last_leg_item = r_t_index
-    #     last_leg_name = last_leg_name_cmp_r
-    # else:
-    #     last_leg_item = l_t_index
-    #     last_leg_name = last_leg_name_cmp_l
-    # # print(f"This is last leg item {old_last_leg_item}")
 
     # base bone section
     # base order: 上半身, 下半身, 腰 (b_1), グルーブ, センター, 全ての親
@@ -293,12 +212,9 @@ def main():
     # pelvis_pos = [-4.999999873689376e-06, 38.566917419433594, -0.533614993095398]
 
     # adding IK and such
-    # leg_left_obj = retme.bones[last_leg_item + l_l]
-    # leg_left_knee_obj = retme.bones[last_leg_item + l_k]
+
     leg_left_ankle_obj = retme.bones[l_a_index]
     leg_left_toe_obj = retme.bones[l_t_index]
-    # leg_right_obj = retme.bones[last_leg_item + r_l]
-    # leg_right_knee_obj = retme.bones[last_leg_item + r_k]
     leg_right_ankle_obj = retme.bones[r_a_index]
     leg_right_toe_obj = retme.bones[r_t_index]
 
@@ -324,45 +240,45 @@ def main():
     # if name == "ValveBiped.Bip01_R_Toe0":
     #     retme.bones.insert(last_leg_item + 1, )
 
-    leg_left_ik_obj = pmxstruct.PmxBone(leg_left_ik_name, "", leg_left_ankle_pos, last_bone + 5, 0, False,
+    leg_left_ik_obj = pmxstruct.PmxBone(leg_left_ik_name, "", leg_left_ankle_pos, last_bone_index + 5, 0, False,
                                         True, True, True, True, True,
                                         False, [0.0, 0.0, 0.0], False, False, False,
                                         False, False, None, None, None, None, None, None,
                                         l_a_index, 40, 114.5916,
-                                        [[l_k_index, knee_limit_1, knee_limit_2],
-                                         [l_l_index, None, None]])
-    retme.bones.insert(last_bone + 1, leg_left_ik_obj)
+                                        [pmxstruct.PmxBoneIkLink(idx=l_k_index, limit_min=knee_limit_1, limit_max=knee_limit_2),
+                                         pmxstruct.PmxBoneIkLink(idx=l_l_index)])
+    insert_single_bone()
+    retme.bones.insert(last_bone_index + 1, leg_left_ik_obj)
 
-    leg_left_toe_ik_obj = pmxstruct.PmxBone(leg_left_toe_ik_name, "", leg_left_toe_pos, last_bone + 1, 0,
+    leg_left_toe_ik_obj = pmxstruct.PmxBone(leg_left_toe_ik_name, "", leg_left_toe_pos, last_bone_index + 1, 0,
                                             False,
                                             True, True, True, True, True,
                                             False, [0, 0, 0], False, False, False,
                                             False, False, None, None, None, None, None, None,
-                                            l_t_index, 3, 229.1831, [[l_a_index, None, None]])
-    retme.bones.insert(last_bone + 2, leg_left_toe_ik_obj)
+                                            l_t_index, 3, 229.1831, [pmxstruct.PmxBoneIkLink(idx=l_a_index)])
+    retme.bones.insert(last_bone_index + 2, leg_left_toe_ik_obj)
 
-    leg_right_ik_obj = pmxstruct.PmxBone(leg_right_ik_name, "", leg_right_ankle_pos, last_bone + 5, 0,
+    leg_right_ik_obj = pmxstruct.PmxBone(leg_right_ik_name, "", leg_right_ankle_pos, last_bone_index + 5, 0,
                                          False,
                                          True, True, True, True, True,
                                          False, [0.0, 0.0, 0.0], False, False, False,
                                          False, False, None, None, None, None, None, None,
                                          r_a_index, 40, 114.5916,
-                                         [[r_k_index, knee_limit_1, knee_limit_2],
-                                          [r_l_index, None, None]])
-    retme.bones.insert(last_bone + 3, leg_right_ik_obj)
+                                         [pmxstruct.PmxBoneIkLink(idx=r_k_index, limit_min=knee_limit_1, limit_max=knee_limit_2),
+                                          pmxstruct.PmxBoneIkLink(idx=r_l_index)])
+    retme.bones.insert(last_bone_index + 3, leg_right_ik_obj)
 
-    leg_right_toe_ik_obj = pmxstruct.PmxBone(leg_right_toe_ik_name, "", leg_right_toe_pos, last_bone + 3, 0,
+    leg_right_toe_ik_obj = pmxstruct.PmxBone(leg_right_toe_ik_name, "", leg_right_toe_pos, last_bone_index + 3, 0,
                                              False,
                                              True, True, True, True, True,
                                              False, [0, 0, 0], False, False, False,
                                              False, False, None, None, None, None, None, None,
-                                             r_t_index, 3, 229.1831, [[r_a_index, None, None]])
-    retme.bones.insert(last_bone + 4, leg_right_toe_ik_obj)
+                                             r_t_index, 3, 229.1831, [pmxstruct.PmxBoneIkLink(idx=r_a_index)])
+    retme.bones.insert(last_bone_index + 4, leg_right_toe_ik_obj)
 
     # # base part
     b4_pos = [0, 0, 0]
 
-    # for some reasons, if we pass value from pelvis_pos to b3_pos, pelvis_pos will change as well?
     b3_pos = [-4.999999873689376e-06, 21, -0.533614993095398]
     b2_pos = b3_pos
     b1_pos = [-4.999999873689376e-06, 32, -0.533614993095398]
@@ -381,31 +297,31 @@ def main():
                                None, False, None, None, None, None, None, None,
                                None, None, None, None
                                )
-    retme.bones.insert(last_bone + 5, b4_obj)
+    retme.bones.insert(last_bone_index + 5, b4_obj)
 
-    b3_obj = pmxstruct.PmxBone(b3_name, "", b3_pos, last_bone + 5, 0, False,
+    b3_obj = pmxstruct.PmxBone(b3_name, "", b3_pos, last_bone_index + 5, 0, False,
                                True, True, True, True, False,
                                False, [0, 0, 0], False, False, None,
                                None, False, None, None, None, None, None, None,
                                None, None, None, None
                                )
-    retme.bones.insert(last_bone + 6, b3_obj)
+    retme.bones.insert(last_bone_index + 6, b3_obj)
 
-    b2_obj = pmxstruct.PmxBone(b2_name, "", b2_pos, last_bone + 6, 0, False,
+    b2_obj = pmxstruct.PmxBone(b2_name, "", b2_pos, last_bone_index + 6, 0, False,
                                True, True, True, True, False,
                                False, [0, 0, 0], False, False, None,
                                None, False, None, None, None, None, None, None,
                                None, None, None, None
                                )
-    retme.bones.insert(last_bone + 7, b2_obj)
+    retme.bones.insert(last_bone_index + 7, b2_obj)
 
-    b1_obj = pmxstruct.PmxBone(b1_name, "", b1_pos, last_bone + 7, 0, False,
+    b1_obj = pmxstruct.PmxBone(b1_name, "", b1_pos, last_bone_index + 7, 0, False,
                                True, False, True, True, False,
                                False, [0, 0, 0], False, False, None,
                                None, False, None, None, None, None, None, None,
                                None, None, None, None
                                )
-    retme.bones.insert(last_bone + 8, b1_obj)
+    retme.bones.insert(last_bone_index + 8, b1_obj)
 
     output_filename_pmx = input_filename_pmx[0:-4] + "_sourcetrans.pmx"
     pmxlib.write_pmx(output_filename_pmx, retme, moreinfo=moreinfo)
