4379f0ffa5d2d37232b81ef89c614307595a1a76
Brian Henson <brian.henson1@yahoo.com>
2021-12-21 12:09:00 -0800

    transfer_morph_to_nearest: make it a bit better for most cases

--- mmd_scripting/scratch_stuff/transfer_morph_to_nearest.py
+++ mmd_scripting/scratch_stuff/transfer_morph_to_nearest.py
@@ -6,8 +6,8 @@ _SCRIPT_VERSION = "Script version:  Nuthouse01 - v0.6.00 - 6/10/2021"
 
 
 THRESHOLD = 0.002
-
-
+WIGGLE_ROOM = 0.1
+EPSILON = 1e-7
 
 def main():
 	print("Transfer a morph from one model to another, assuming the geometry is in the same position")
@@ -50,7 +50,10 @@ def main():
 		# begin iterating!
 		# for each vert ref in vertex morph, go to vert in source PMX to get position
 		#
+		print("running...")
 		already_used_verts = set()
+		
+		stats_nearest_distance_for_all_verts = []
 		
 		for asdf, morphitem in enumerate(source_morph.items):
 			core.print_progress_oneline(asdf / len(source_morph.items))
@@ -67,18 +70,47 @@ def main():
 			
 			# calculate dist from here to each vert in dest_pmx
 			# find all verts within this dist threshold
-			matching_verts = []
-			dist_list = []
+			nearest_vert_dist = 1000
+			short_dist_list = []
+			# first, find every vertex within 0.1 units (just so i have a shorter list to sort)
 			for d,v2 in enumerate(dest_pmx.verts):
-				dist = core.my_euclidian_distance([vertpos[i] - v2.pos[i] for i in range(3)])
-				dist_list.append(dist)
-				if dist < THRESHOLD:
-					matching_verts.append(d)
-			if not matching_verts:
-				print("warning: unable to find any verts within the threshold for source vert ID %d" % vertid)
-				print("nearest vert is dist=%f" % min(dist_list))
+				dist = core.my_euclidian_distance([a-b for a,b in zip(vertpos, v2.pos)])
+				nearest_vert_dist = min(nearest_vert_dist, dist)
+				# dist = core.my_euclidian_distance([vertpos[i] - v2.pos[i] for i in range(3)])
+				if dist < 0.01:
+					short_dist_list.append((dist, d))
+			# if nothing is found, then maybe give some insight for why?
+			if not short_dist_list:
+				print("warning: unable to find any verts within the threshold for source vert ID %d, nearest vert is dist=%f" % (vertid, nearest_vert_dist))
+				continue
+			# then sort this list to find the smallest distance
+			short_dist_list.sort(key=lambda x: x[0])
+			nearest_vert_dist = short_dist_list[0][0]
+			# accumulate for stats before applying wiggle room
+			stats_nearest_distance_for_all_verts.append(nearest_vert_dist)
+			# apply the wiggle room, and also set it to epsilon if less than that, so the dist is never exactly 0
+			nearest_vert_dist *= WIGGLE_ROOM + 1
+			nearest_vert_dist = max(nearest_vert_dist, EPSILON)
+			# now get all vertices that are less than this compare value
+			# i.e. get all vertices that are between (nearest) and (nearest + 10%)
+			matching_verts = []
+			for dist, v in short_dist_list:
+				if dist <= nearest_vert_dist:
+					matching_verts.append(v)
+				else:
+					break
 			for v in matching_verts:
-				if v not in already_used_verts:
+				if v in already_used_verts:
+					# if the vertex has already been used, and the previous match wants to move it by a different amount
+					#  than the current match, then i've got a problem...
+					#  if both matches want to move by the same amount, then stay quiet
+					already_used_morphitem = core.my_list_search(newmorph.items, lambda x: x.vert_idx == v, getitem=True)
+					if already_used_morphitem.move != morphitem.move:
+						print(f'warning: disagreement on how to move vertex {v}')
+				else:
+					# if it was not yet used, then use it!
+					# morphitem.vert_idx === v
+					# copy the way that the source morph wants to move this vert.
 					already_used_verts.add(v)
 					newitem = pmxstruct.PmxMorphItemVertex(vert_idx=v, move=morphitem.move)
 					newmorph.items.append(newitem)
@@ -93,15 +125,22 @@ def main():
 			print("warning: length mismatch! source has %d and new has %d, this requires closer attention" %
 				  (len(source_morph.items), len(newmorph.items)))
 		
+		# stats! how close do the two models correlate?
+		min_dist = min(stats_nearest_distance_for_all_verts)
+		max_dist = max(stats_nearest_distance_for_all_verts)
+		avg_dist = sum(stats_nearest_distance_for_all_verts) / len(stats_nearest_distance_for_all_verts)
+		print("stats: min_dist = %.8f, max_dist = %.8f, avg_dist = %.8f" % (min_dist, max_dist, avg_dist))
 		# add it to the dest pmx
 		dest_pmx.morphs.append(newmorph)
 		
 		pass  # end of while-loop
 	
 	
+	output_filename_pmx = core.filepath_insert_suffix(dest_name_pmx, "_MORPHTRANSFER")
+	output_filename_pmx = core.filepath_get_unused_name(output_filename_pmx)
+	pmxlib.write_pmx(output_filename_pmx, dest_pmx)
 	print("DONE")
-	pmxlib.write_pmx("TRANSFER.pmx", dest_pmx)
-	
+
 	return None
 
 if __name__ == '__main__':
